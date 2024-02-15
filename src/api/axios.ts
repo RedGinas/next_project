c4f84362de0524f1ca9ad7953f38477dfe8e8ff0
RedGinas <snezhkov.r@gmail.com>
2024-02-15 10:38:01 -0500

    update api url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,3 +1,5 @@
 import axios from 'axios';
 
-export const axiosClient = axios.create({ baseURL: 'http://localhost:4000/' });
+export const axiosClient = axios.create({
+  baseURL: 'https://next-back-5b0519cbe113.herokuapp.com/',
+});
